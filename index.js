cd20df6a518939ccb122f2d4a181bc1d633ac233
mopig <mopig@foxmail.com>
2014-04-03 18:41:36 +0800

    下班前 提交

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var serveStatic = require('serve-static'),
     connect = require('connect'),
     app = connect(),
+    path = require('path'),
     makeJade = require('./lib/processor/jade.js'),
     makeLess = require('./lib/processor/less.js');
 
@@ -8,6 +9,11 @@ module.exports = function(dirname) {
     app.use(function(req, res, next){
         if (req.url == '/'){
             req.url = '/index.html';
+            next();
+        } else if (path.extname(req.url)=='.jade' || path.extname(req.url)=='.less') {
+            res.writeHead(404, {});
+            res.end();
+        } else {
             next();
         }
     })
